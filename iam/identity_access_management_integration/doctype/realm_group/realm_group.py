07eb34d9cf0901071d8ee272c9b0e063356a2a02
rome <krmediante27@gmail.com>
2023-11-18 14:11:21 +0800

    Update fields

---
+++ iam/identity_access_management_integration/doctype/realm_group/realm_group.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Kid Mediante and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+
+class RealmGroup(NestedSet):
+	pass
