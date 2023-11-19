b306e6e9f7048ff6047e17483eb620afcf2156b1
rome <krmediante27@gmail.com>
2023-11-20 06:19:54 +0800

    Remove no longer required fields

--- iam/www/login.py
+++ iam/www/login.py
@@ -67,7 +67,6 @@ def get_context(context):
             "custom_realm_name",
             "custom_provider_label",
             "custom_keycloak_options",
-            "custom_iam_child_system",
             "custom_iam_portal_login",
             "base_url",
         ],
@@ -97,7 +96,6 @@ def get_context(context):
                     "label": provider.custom_provider_label,
                     "base_url": provider.base_url,
                     "options": provider.custom_keycloak_options,
-                    "child_system": provider.custom_iam_child_system,
                     "iam_portal_login": provider.custom_iam_portal_login,
                     "icon": icon,
                 }
